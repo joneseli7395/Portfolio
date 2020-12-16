385389f15897479554e219b4eef8424ac2f95658
Eli Jones <joneseli7395@gmail.com>
2020-12-16 13:38:50 -0500

    welp..

--- PortfolioTemp/README.md
+++
@@ -1,3 +0,0 @@
-# Portfolio
- 
-[![Netlify Status](https://api.netlify.com/api/v1/badges/a34e94c4-9864-45e4-acab-4fb47f5df267/deploy-status)](https://app.netlify.com/sites/ej-dragon-website/deploys)
--- PortfolioTemp/Scripts/email.js
+++
@@ -1,5 +0,0 @@
-﻿<type="text/javascript"src = "https://cdn.jsdelivr.net/npm/emailjs-com@2/dist/email.min.js" >
-    <type="text/javascript" >
-        (function () {
-            emailjs.init("user_ajuUnKWeBLumXDMuiYhbp");
-        })();
