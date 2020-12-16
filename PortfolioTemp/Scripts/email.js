df65fbd20271839f48dd67dfada345bd08f74271
Eli Jones <joneseli7395@gmail.com>
2020-12-16 13:40:25 -0500

    again

---
+++ PortfolioTemp/Scripts/email.js
@@ -0,0 +1,5 @@
+﻿<type="text/javascript"src = "https://cdn.jsdelivr.net/npm/emailjs-com@2/dist/email.min.js" >
+    <type="text/javascript" >
+        (function () {
+            emailjs.init("user_ajuUnKWeBLumXDMuiYhbp");
+        })();
